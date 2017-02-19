b8cefe7f798a591e91e9bc4621360a43aef8c7a9
Anurag Khandelwal <anuragk@berkeley.edu>
2017-02-18 17:21:37 -0800

    Removed unnecessary commandline args

--- src/main/java/edu/berkeley/cs/neo4jbench/path/PathBench.java
+++ src/main/java/edu/berkeley/cs/neo4jbench/path/PathBench.java
@@ -14,9 +14,6 @@ import java.io.PrintWriter;
 public class PathBench {
   static String outputFile;
   static String dbPath;
-  static int numWarmupQueries, numMeasureQueries;
-  static int numClients;
-  static boolean tuned;
   static String pageCache;
   static String query;
 
@@ -24,11 +21,7 @@ public class PathBench {
     dbPath = args[0];
     String queryFile = args[1];
     outputFile = queryFile + ".result";
-    numWarmupQueries = Integer.parseInt(args[2]);
-    numMeasureQueries = Integer.parseInt(args[3]);
-    numClients = Integer.parseInt(args[4]);
-    tuned = Boolean.valueOf(args[5]);
-    pageCache = args[6];
+    pageCache = args[2];
 
     query = BenchUtils.readPathQuery(queryFile);
 
@@ -42,13 +35,9 @@ public class PathBench {
   private static void benchLatency() throws IOException {
     GraphDatabaseService db;
     System.out.println("About to open database");
-    if (tuned) {
-      db = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(dbPath)
-        .setConfig(GraphDatabaseSettings.cache_type, "none")
-        .setConfig(GraphDatabaseSettings.pagecache_memory, pageCache).newGraphDatabase();
-    } else {
-      db = new GraphDatabaseFactory().newEmbeddedDatabase(dbPath);
-    }
+    db = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(dbPath)
+      .setConfig(GraphDatabaseSettings.cache_type, "none")
+      .setConfig(GraphDatabaseSettings.pagecache_memory, pageCache).newGraphDatabase();
     BenchUtils.registerShutdownHook(db);
     System.out.println("Done opening");
 
